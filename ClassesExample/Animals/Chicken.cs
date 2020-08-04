ab3986faef18511f0723e365459969a8eb17a7d9
Todd Spainhour <toddspainhour@gmail.com>
2020-08-03 20:50:29 -0500

    Add project files.

---
+++ ClassesExample/Animals/Chicken.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesExample.Animals
+{
+    class Chicken
+    {
+
+        //property - for data you want to expose (accessibility modifier -> type of thing it's storing, -> Name -> Read and Write permissions)
+        public string Name { get; set; }
+
+
+
+
+        // field keeping info close.just for you to use. Only this class cares about
+        private bool _isHungry = true;
+
+
+
+
+        // constructor 
+        public Chicken(string name) // constructor paramenters - an argument here names something required
+        {
+            Name = name;
+            Console.WriteLine("Chicken inside default constructor");
+        }
+
+
+
+        // method (signature = accessibility modifier -> return type -> Name -> Parameters)
+        public void Peck(string typeOfFood)
+        {
+            if (_isHungry)
+            {
+                Console.WriteLine($"{Name} pecked hungrily at the {typeOfFood}");
+                _isHungry = false;
+            }
+            else 
+            {
+                Console.WriteLine($"{Name} is not hungry.");
+            }
+
+        }
+
+
+    }
+}
+
+
+
+// every first letter in a class and method name needs to be capitalized (title case, no underscores _)
+// default constructor has no return type, what's the minimum amout of info we need to get started
+// public Chicken()
+// {
+// }
+
+// default constructor
+
+
+// properties store data in memory to use
+
+// accessibility modifier - who can gain access (public), 
+// return type - what does this method return (void means nothing),
+// name (Peck)
+// last part accepts arguments or could be empty parenthasis (string typeOfFood)
+// and curly braces for your block of code
+// all 4 of those things make this method uniniqe 
---
+++ ClassesExample/Program.cs
@@ -0,0 +1,48 @@
+﻿using ClassesExample.Animals;
+using System;
+
+
+namespace ClassesExample
+
+{
+    class Program
+        
+    {
+        
+        static void Main(string[] args)
+        {
+
+            var chicken = new Chicken("Liza");
+            var bob = new Chicken("Bob");
+
+            chicken.Name = "Liza";
+            // bob.Name = "Bob";
+
+            chicken.Peck("corn");
+            bob.Peck("Watermelon");
+
+            Console.WriteLine($"{chicken.Name} and {bob.Name} are proud chickens.");
+
+
+
+            Console.Read();
+
+        }
+
+        
+
+    }
+
+}
+
+
+
+//' using System' lives inside the eco system Microsoft built. Any code that's inside this namespace is avaviable. it's to hold related code
+// a namespace is a large high-level bundle of code
+// A class is a like a blueprint for functionality. classes in C# and JS are similar. there is nothing special about the word program above 
+// once class per file and name the .cs file the same as that class
+// below is a method, a function inside a class, a behavior
+// "new it up." is creating a new instance
+// hitting tab to auto complete adds the 'using' up top
+// new Chicken () - the parenthesis says give me a copy of this. called a constructor
+// without making it a variable it is constructed to memory and instantly goes away
